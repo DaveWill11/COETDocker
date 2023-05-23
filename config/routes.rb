c890d30d8f3ccd832e4a90bb3f2c18830ba9005f
Dave <djewilliams11@gmail.com>
2023-05-23 21:21:45 +0000

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get 'items/index'
+  get 'api/items', to:'items#index'
+  get 'api/items/:id', to: 'items#show'
+  post'api/items', to: 'items#create'
+  get 'api/items/edit/:id', to: 'items#update'
+  get 'api/items/delete/:id', to: 'items#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
